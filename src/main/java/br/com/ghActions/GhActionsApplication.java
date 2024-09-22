79ca902fa8bfb2f9527e38cd6e6f37a789d3f8f2
maxjdev <maxjramosdev@gmail.com>
2024-09-22 14:50:32 -0300

    add gh actions

---
+++ src/main/java/br/com/ghActions/GhActionsApplication.java
@@ -0,0 +1,20 @@
+package br.com.ghActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GhActionsApplication {
+
+	@GetMapping
+	public String home() {
+		return "<h1>Hello World!</h1>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhActionsApplication.class, args);
+	}
+}
